aa91ce1218401d6b9f2ea11dc7db876e4dbc6a68
JE <JE@Slasher-Elan>
2011-08-16 00:05:52 +0800

    Savepoint

--- workspace/DxDroid/src/com/android/dxdroid/DxDroid.java
+++ workspace/DxDroid/src/com/android/dxdroid/DxDroid.java
@@ -1,23 +1,3 @@
-/* Application demonstrates the interaction between Seeeduino ADK and Android Device
- * using Niels Brouwers' MicroBridge library. 
- * 
- * Android Device: Any device with Android v1.5 which supports ADB(Android Debug Bridge).   
- *  
- * This application uses a very simple (or a trivial) design to make it understandable.
- * 
- * Overview:
- * 1.Seeeduino ADK Main Board periodically samples Analog Channel 0 and sends it
- *   to Android Device for display. This value is displayed using a TextView and SeekBar Widgets
- * 
- * 2.Android device controls the state of a LED connected to Digital Pin 12 of ADK Main Board.
- *   A Button Widget used for this.
- * 
- * Microbridge uses ADB based client-server implementation. The Server code that runs on Android
- * device runs in a separate thread. Hence any update to UI widgets value has to be carried out 
- * in UI thread. This application uses XML based UI creation as it is easier for adding addition
- * UI Widgets. 
- * 
- */
 package com.android.dxdroid;
 
 import java.io.IOException;
@@ -33,7 +13,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class DxDroid extends Activity implements OnClickListener {
 	private int adcSensorValue = 10;
@@ -49,6 +28,19 @@ public class DxDroid extends Activity implements OnClickListener {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.spo2_layout);
 
+//		Display display = getWindowManager().getDefaultDisplay();
+//		int width = display.getWidth();
+//		int height = display.getHeight();
+//
+//		setContentView(R.layout.main);
+//		
+//		RelativeLayout screen = (RelativeLayout)findViewById(R.id.screen);
+//		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(width-20,height-20);
+//		
+//		final DrawView drawview = new DrawView(this, height, width);
+//		params.setMargins(10, 10, 10, 10);
+//		screen.addView(drawview,params);
+		
 		// Create TCP server (based on MicroBridge LightWeight Server)
 		try {
 			server = new Server(4568); // Use the same port number used in
@@ -68,15 +60,15 @@ public class DxDroid extends Activity implements OnClickListener {
 				if (data.length < 2)
 					return;
 				adcSensorValue = (data[0] & 0xff) | ((data[1] & 0xff) << 8);
-
+//				drawview.getData(new String[]{"","0",""+adcSensorValue});
+				
 				// Any update to UI can not be carried out in a non UI thread
 				// like the one used
 				// for Server. Hence runOnUIThread is used.
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						new UpdateData().execute(adcSensorValue);
-
+						new UpdateData().execute(adcSensorValue);						
 					}
 				});
 
