937b8f4bf688285fd42e778025dbb7654daab7e6
JE <JE@Slasher-Elan>
2011-08-16 00:05:52 +0800

    Savepoint

--- workspace/DxDroid/gen/com/android/dxdroid/R.java
+++ workspace/DxDroid/gen/com/android/dxdroid/R.java
@@ -14,20 +14,25 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int SPO2HeartRateValue=0x7f050000;
-        public static final int SPO2OxygenSaturationValue=0x7f050001;
+        public static final int SPO2HeartRateValue=0x7f060002;
+        public static final int SPO2OxygenSaturationValue=0x7f060003;
+        public static final int linearLayout1=0x7f060001;
+        public static final int screen=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int spo2_layout=0x7f030001;
     }
+    public static final class raw {
+        public static final int ecg=0x7f040000;
+    }
     public static final class string {
-        public static final int SPO2HeartRateLabel=0x7f040003;
-        public static final int SPO2HeartRateUnit=0x7f040004;
-        public static final int SPO2Label=0x7f040002;
-        public static final int SPO2OxygenSaturationLabel=0x7f040005;
-        public static final int SPO2OxygenSaturationUnit=0x7f040006;
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int SPO2HeartRateLabel=0x7f050003;
+        public static final int SPO2HeartRateUnit=0x7f050004;
+        public static final int SPO2Label=0x7f050002;
+        public static final int SPO2OxygenSaturationLabel=0x7f050005;
+        public static final int SPO2OxygenSaturationUnit=0x7f050006;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
