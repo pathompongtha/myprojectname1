0a180141e3e518a4a785fb77ed5c36badbe6f9b5
JE <JE@Slasher-Elan>
2011-09-13 16:06:44 +0800

    Hardcoded email addresses

--- workspace/RxEmail2/src/com/dxdroid/email/RxEmail2Activity.java
+++ workspace/RxEmail2/src/com/dxdroid/email/RxEmail2Activity.java
@@ -70,16 +70,19 @@ public class RxEmail2Activity extends Activity {
             	Toast.makeText(getApplicationContext(), "Sending ... ", Toast.LENGTH_SHORT).show();
                 try {   
                     GMailSender sender = new GMailSender(senderAddress.getText().toString(), pass.getText().toString());
-//                    sender.sendMail(subject.getText().toString(),
-//                    		body.getText().toString(),
-//                    		senderAddress.getText().toString(),
-//                    		address.getText().toString());
-
-                    sender.sendMail(subject.getText().toString(),
-                    		body.getText().toString(),
-                    		senderAddress.getText().toString(),
-                    		address.getText().toString(),
-                    		attachment);
+                    if(attachment == null) {
+                      sender.sendMail(subject.getText().toString(),
+                		body.getText().toString(),
+                		senderAddress.getText().toString(),
+                		address.getText().toString());
+                    	
+                    } else {
+	                    sender.sendMail(subject.getText().toString(),
+	                    		body.getText().toString(),
+	                    		senderAddress.getText().toString(),
+	                    		address.getText().toString(),
+	                    		attachment);
+                    }
                     Toast.makeText(getApplicationContext(), "Sent",Toast.LENGTH_SHORT).show();
                 } catch (Exception e) {   
                     Log.e("SendMail", e.getMessage(), e);   
